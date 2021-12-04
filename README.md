327fb42e5b17c57db9d4236af75d1bcf8eb9e625
Thomas Fowler <ThomasJamesFowler97@gmail.com>
2021-12-04 21:44:34 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[Great Southern Bank](https://www.greatsouthernbank.com.au/) (Formerly CUA) unfortunately does not provide an api
+for accessing and managing your bank account automatically.
+
+This is a simple api using [Playwright](https://playwright.dev/) to control GSB's online banking website
+for such reasons.
---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.serialization") version "1.6.0"
+}
+
+group = "com.tfowl.gsb"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
+
+    implementation("com.microsoft.playwright:playwright:1.17.1")
+
+    implementation("org.jsoup:jsoup:1.14.3")
+
+    implementation("com.jakewharton.picnic:picnic:0.5.0")
+
+    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.13")
+    implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:1.1.13")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+    implementation("de.brudaswen.kotlinx.serialization:kotlinx-serialization-csv:2.0.0")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
