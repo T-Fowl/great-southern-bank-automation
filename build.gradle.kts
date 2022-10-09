c1624b1538dd6f5873a9cb31a58ba20d8ea3f443
Thomas <thomasjamesfowler97@gmail.com>
2022-10-10 04:20:34 +0800

    Upgraded all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.7.20"
 }
 
 group = "com.tfowl.gsb"
@@ -13,16 +13,15 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
+    implementation("org.jsoup:jsoup:1.15.3")
     implementation("com.microsoft.playwright:playwright:1.27.0")
 
-    implementation("org.jsoup:jsoup:1.14.3")
+    implementation("com.jakewharton.picnic:picnic:0.6.0")
 
-    implementation("com.jakewharton.picnic:picnic:0.5.0")
-
-    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.13")
-    implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:1.1.13")
+    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.16")
+    implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:1.1.16")
 
     implementation("org.jetbrains.kotlinx:dataframe:0.9.0-dev-1139")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
