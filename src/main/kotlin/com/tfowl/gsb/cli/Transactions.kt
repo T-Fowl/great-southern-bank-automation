768282fb6c3f48ef147c44717f3c8743647c5eaa
Thomas <thomasjamesfowler97@gmail.com>
2022-10-10 17:59:09 +0800

    Added output location and format options to Transactions command

--- src/main/kotlin/com/tfowl/gsb/cli/Transactions.kt
+++ src/main/kotlin/com/tfowl/gsb/cli/Transactions.kt
@@ -1,18 +1,63 @@
 package com.tfowl.gsb.cli
 
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.defaultStdout
+import com.github.ajalt.clikt.parameters.types.enum
+import com.github.ajalt.clikt.parameters.types.outputStream
 import com.github.michaelbull.result.unwrap
+import com.jakewharton.picnic.Table
+import com.jakewharton.picnic.renderText
+import com.jakewharton.picnic.table
 import com.tfowl.gsb.GSBMember
 import com.tfowl.gsb.model.AccountNumber
+import com.tfowl.gsb.model.Transaction
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.forEach
+import org.jetbrains.kotlinx.dataframe.io.html
+import org.jetbrains.kotlinx.dataframe.io.writeCSV
+import org.jetbrains.kotlinx.dataframe.io.writeJson
+
+enum class TransactionOutputFormat {
+    Csv,
+    Json,
+    Html,
+    Table
+}
+
+private fun DataFrame<Transaction>.toTable(): Table = table {
+    header {
+        row {
+            columnNames().forEach { cell(it) }
+        }
+    }
+    body {
+        forEach { dr ->
+            row {
+                dr.values().forEach { cell(it) }
+            }
+        }
+    }
+}
 
 class Transactions : GsbSubcommand() {
 
     private val account by option().required()
 
+    private val output by option().outputStream().defaultStdout()
+    private val format by option().enum<TransactionOutputFormat>().default(TransactionOutputFormat.Csv)
+
     override fun run(member: GSBMember) {
         val transactions = member.transactions(AccountNumber(account)).unwrap()
 
-        println(transactions)
+        output.bufferedWriter().use { writer ->
+            when (format) {
+                TransactionOutputFormat.Csv   -> transactions.writeCSV(writer)
+                TransactionOutputFormat.Json  -> transactions.writeJson(writer)
+                TransactionOutputFormat.Html  -> writer.append(transactions.html())
+                TransactionOutputFormat.Table -> writer.append(transactions.toTable().renderText())
+            }
+        }
     }
 }
